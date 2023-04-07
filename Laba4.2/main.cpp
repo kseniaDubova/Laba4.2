245fb1215016bc15a1135c4288657acd24988c24
kseniaDubova <ksushadubova@icloud.com>
2023-04-07 23:34:19 +0400

    сортировки для vector, подсчет копирований и сравнений

--- Laba4.2/main.cpp
+++ Laba4.2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct stats
@@ -10,13 +11,13 @@ struct stats
 
 size_t lcg(){
     static size_t x = 0;
-    x = (1021*x+24631) % 116640;
+    x = (1021*x + 24631) % 116640;
     return x;
 }
 
 void clear_monitor()
 {
-    for (int i=0; i<31; i++)
+    for (int i = 0; i < 31; i++)
     {
         cout << endl;
     }
@@ -36,37 +37,55 @@ int check()
 }
 
 
-void inserts_sort(int* arr, int size)
+void inserts_sort(vector<int>& arr, stats& count)
 {
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j > 0 && arr[j - 1] > arr[j]; j--) {
-            int tmp = arr[j];
-            arr[j] = arr[j - 1];
-            arr[j - 1] = tmp;
+    for (auto i = arr.begin(); i != arr.end() - 1; i++)
+    {
+        count.comparison_count += 1;
+        for (auto j = i + 1; j != arr.begin() && *(j - 1) > *j; j--)
+        {
+            count.comparison_count += 2;
+            int tmp = *j;
+            *j = *(j - 1);
+            *(j - 1) = tmp;
+            count.copy_count += 3;
         }
     }
 }
 
 
-void shaker_sort(int* arr, int size) {
-    int left = 0, right = size, swap_flag = 1;
-    while ((left < right) && (swap_flag > 0)) {
-        swap_flag = 0;
-        for (int i = left; i < right; i++) {
-            if (arr[i] > arr[i + 1]) {
-                int tmp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = tmp;
-                swap_flag = 1;
+void shaker_sort(vector<int>& arr, stats& count)
+{
+    auto left = arr.begin()+1;
+    auto right = arr.end()-1;
+    int swap_flag = 1;
+    while (left != right)
+    {
+        count.comparison_count += 1;
+        for (auto i = left; i <= right; i++)
+        {
+            count.comparison_count += 1;
+            if (*i < *(i - 1))
+            {
+                count.comparison_count += 1;
+                int tmp = *i;
+                *i = *(i - 1);
+                *(i - 1) = tmp;
+                count.copy_count += 3;
             }
         }
         right--;
-        for (int i = right; i > left; i--) {
-            if (arr[i] < arr[i - 1]) {
-                int tmp = arr[i];
-                arr[i] = arr[i - 1];
-                arr[i - 1] = tmp;
+        for (auto i = right; i >= left; i--)
+        {
+            count.comparison_count += 1;
+            if (*i < *(i - 1))
+            {
+                count.comparison_count += 1;
+                int tmp = *i;
+                *i = *(i - 1);
+                *(i - 1) = tmp;
                 swap_flag = 1;
+                count.copy_count += 3;
             }
         }
         left++;
@@ -92,6 +111,23 @@ int muny_1()
     return n;
 }
 
+void add_arr(vector<int>& arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr.push_back(int(lcg()));
+    }
+}
+
+
+void print_v(vector<int>& arr)
+{
+    for (auto iter = arr.begin(); iter != arr.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+}
+
 
 void muny_2()
 {
@@ -114,11 +150,17 @@ void muny_2()
         cout << "Incorrect value" << endl << "Operation №";
         size = check();
     }
+    vector<int> arr;
+    add_arr(arr, size);
     clear_monitor();
 }
 
 
 int main()
 {
-    
+    vector<int> j = {4,6,5,9,1,7,8,3,2,10};
+    stats g;
+    shaker_sort(j, g);
+    print_v(j);
+    cout << g.comparison_count << "\t" << g.copy_count;
 }
